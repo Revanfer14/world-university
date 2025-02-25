136599490d6215dfd45121c9e02454b68cd51eb1
Revan Ferdinand <revancrozz@gmail.com>
2025-02-26 02:27:41 +0700

    Fix mark di maps

--- components/Maps.jsx
+++ components/Maps.jsx
@@ -1,5 +1,15 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon from "../public/marker.png";
+
+const customMarker = new L.Icon({
+  iconUrl: markerIcon,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
 
 const CountryMap = ({ lat, lon, name }) => (
   <MapContainer
@@ -12,7 +22,7 @@ const CountryMap = ({ lat, lon, name }) => (
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     />
-    <Marker position={[lat, lon]}>
+    <Marker position={[lat, lon]} icon={customMarker}>
       <Popup>{name}</Popup>
     </Marker>
   </MapContainer>
